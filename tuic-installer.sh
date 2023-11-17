d63f5663c65d5635ab561d59500f73fddb67b98b
deathline94 <dlb4all@gmail.com>
2023-11-17 14:29:31 +0330

    v1.1-fixed broken repo

--- tuic-installer.sh
+++ tuic-installer.sh
@@ -115,10 +115,10 @@ detect_arch() {
 }
 
 server_arch=$(detect_arch)
-latest_release_version=$(curl -s "https://api.github.com/repos/EAimTY/tuic/releases/latest" | jq -r ".tag_name")
+latest_release_version=$(curl -s "https://api.github.com/repos/etjec4/tuic/releases/latest" | jq -r ".tag_name")
 
 # Build the download URL based on the latest release version and detected architecture
-download_url="https://github.com/EAimTY/tuic/releases/download/$latest_release_version/$latest_release_version-$server_arch"
+download_url="https://github.com/etjec4/tuic/releases/download/$latest_release_version/$latest_release_version-$server_arch"
 
 # Download the binary with verbose output
 mkdir -p /root/tuic
